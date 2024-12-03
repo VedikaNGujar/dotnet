528add6d1b54ac074c3e0ca837990d2b04889ebc
Vedika.Gujar <vedika.gujar@gmail.com>
2024-12-03 19:30:23 +0530

    Reverse String

--- OutputBased/OutputBased/Program.cs
+++ OutputBased/OutputBased/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using OutputBased;
+
 Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine(ReverseString.Approach1("Hello World"));
+Console.WriteLine(ReverseString.Approach2("Hello World"));
--- OutputBased/OutputBased/ReverseString.cs
+++ OutputBased/OutputBased/ReverseString.cs
@@ -11,7 +11,37 @@ namespace OutputBased
 
     //input: hello, output: olleh
     //input: hello world, output: dlrow olleh
-    internal class ReverseString
+    internal static class ReverseString
     {
+        public static string Approach1(string word)
+        {
+            if (string.IsNullOrEmpty(word)) { return word; }
+            else
+            {
+                char[] charArr = word.ToCharArray();
+                for (int i = 0, j = word.Length - 1; i < charArr.Length && j >= 0; i++, j--)
+                {
+                    charArr[i] = word[j];
+                    charArr[j] = word[i];
+                }
+                return new string(charArr);
+
+            }
+        }
+
+
+        public static string Approach2(string word)
+        {
+            if (string.IsNullOrEmpty(word)) { return word; }
+            else
+            {
+                StringBuilder reverse = new StringBuilder();
+                for (int j = word.Length - 1; j >= 0; j--)
+                {
+                    reverse.Append(word[j]);
+                }
+                return reverse.ToString();
+            }
+        }
     }
 }
