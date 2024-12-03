721fae3a0ea3927986644cf51cafc49e9ca778e2
Vedika.Gujar <vedika.gujar@gmail.com>
2024-12-03 19:06:26 +0530

    outputbased

---
+++ OutputBased/OutputBased/ReverseString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputBased
+{
+    //Q.1: How to reverse a string?
+    //Ans.: The user will input a string and the method should return the reverse of that string
+
+    //input: hello, output: olleh
+    //input: hello world, output: dlrow olleh
+    internal class ReverseString
+    {
+    }
+}
