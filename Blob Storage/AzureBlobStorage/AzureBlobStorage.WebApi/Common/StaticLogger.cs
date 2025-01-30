a18f6900ca14abd2b19a35e9d0462d6eb473e71e
Vedika.Gujar <vedika.gujar@gmail.com>
2025-01-30 11:00:27 +0530

    blob

---
+++ Blob Storage/AzureBlobStorage/AzureBlobStorage.WebApi/Common/StaticLogger.cs
@@ -0,0 +1,21 @@
+﻿namespace AzureBlobStorage.WebApi.Common
+{
+    using Serilog;
+
+    namespace AzureBlobStorage.WebApi.Common
+    {
+        public class StaticLogger
+        {
+            public static void EnsureInitialized()
+            {
+                if (Log.Logger is not Serilog.Core.Logger)
+                {
+                    Log.Logger = new LoggerConfiguration()
+                        .Enrich.FromLogContext()
+                        .WriteTo.Console()
+                        .CreateLogger();
+                }
+            }
+        }
+    }
+}
