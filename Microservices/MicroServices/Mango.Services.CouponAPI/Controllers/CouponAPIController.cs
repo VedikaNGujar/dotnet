1aa9f24ab299198ee91bc7bfac060646f47dc5a9
Vedika Gujar <vedika.gujar@gmail.com>
2025-06-11 12:37:31 +0530

    Added CouponController

---
+++ Microservices/MicroServices/Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,50 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController(AppDbContext _appDbContext) : ControllerBase
+    {
+
+        [HttpGet]
+        public ResponseDto<IEnumerable<Coupon>> Get()
+        {
+            var response = new ResponseDto<IEnumerable<Coupon>>();
+            try
+            {
+                response.Result = _appDbContext.Coupons.ToList();
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+            }
+            return response;
+
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto<Coupon> Get(int id)
+        {
+            var response = new ResponseDto<Coupon>();
+
+            try
+            {
+                response.Result = _appDbContext.Coupons.First(x => x.CouponId == id);
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+            }
+            return response;
+
+        }
+    }
+}
--- Microservices/MicroServices/Mango.Services.CouponAPI/Program.cs
+++ Microservices/MicroServices/Mango.Services.CouponAPI/Program.cs
@@ -1,8 +1,17 @@
+using Mango.Services.CouponAPI.Data;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    });
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -22,5 +31,19 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+ApplyMigrations();
 
 app.Run();
+
+
+void ApplyMigrations()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        if (db.Database.GetPendingMigrations().Count() > 0)
+        {
+            db.Database.Migrate();
+        }
+    }
+}
