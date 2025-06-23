e2cc56b35d5eb8b6bdab15af24435f2da2b689a6
Vedika Gujar <vedika.gujar@gmail.com>
2025-06-23 23:19:58 +0530

    productAPI

--- Microservices/MicroServices/Mango.Services.ProductAPI/Program.cs
+++ Microservices/MicroServices/Mango.Services.ProductAPI/Program.cs
@@ -38,7 +38,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseStaticFiles();
 app.MapControllers();
 ApplyMigration();
 app.Run();
--- Microservices/MicroServices/Mango.Web/Service/BaseService.cs
+++ Microservices/MicroServices/Mango.Web/Service/BaseService.cs
@@ -37,18 +37,44 @@ namespace Mango.Web.Service
 
             message.RequestUri = new Uri(requestDto.URL);
 
+
+
             if (withBearer)
             {
                 var token = _tokenProvider.GetToken();
                 message.Headers.Add("Authorization", $"Bearer {token}");
             }
 
-
-            if (requestDto.Data != null)
+            if (requestDto.ContentType == SD.ContentType.MultipartFormData)
             {
+                var content = new MultipartFormDataContent();
 
-                message.Content = new StringContent(JsonConvert.SerializeObject(requestDto.Data), Encoding.UTF8, "application/json");
+                foreach (var prop in requestDto.Data.GetType().GetProperties())
+                {
+                    var value = prop.GetValue(requestDto.Data);
+                    if (value is FormFile)
+                    {
+                        var file = (FormFile)value;
+                        if (file != null)
+                        {
+                            content.Add(new StreamContent(file.OpenReadStream()), prop.Name, file.FileName);
+                        }
+                    }
+                    else
+                    {
+                        content.Add(new StringContent(value == null ? "" : value.ToString()), prop.Name);
+                    }
+                }
+                message.Content = content;
             }
+            else
+            {
+                if (requestDto.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(requestDto.Data), Encoding.UTF8, "application/json");
+                }
+            }
+
 
             HttpResponseMessage response = new();
             switch (requestDto.ApiType)
