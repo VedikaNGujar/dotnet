9f0cb761f2d86ffd7cfdee4a0206cf9d6dc4532e
Vedika Gujar <vedika.gujar@gmail.com>
2025-06-23 23:33:23 +0530

    upload fiile completted

--- Microservices/MicroServices/Mango.Web/Models/ProductDto.cs
+++ Microservices/MicroServices/Mango.Web/Models/ProductDto.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Mango.Web.Utility;
 
 namespace Mango.Web.Models
 {
@@ -11,6 +12,10 @@ namespace Mango.Web.Models
         public string CategoryName { get; set; }
         public string? ImageUrl { get; set; }
         public string? ImageLocalPath { get; set; }
+        
+        
+        [MaxFileSize(1)]
+        [AllowedExtensions(new string[] { ".jpg", ".png" })]
         public IFormFile? Image { get; set; }
 
         [Range(1, 100)]
