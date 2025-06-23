5c1072322e84bd16140dd38a50cb40492660eee2
Vedika Gujar <vedika.gujar@gmail.com>
2025-06-23 16:48:15 +0530

    OrderAPi

--- Microservices/MicroServices/Mango.Services.OrderAPI/Program.cs
+++ Microservices/MicroServices/Mango.Services.OrderAPI/Program.cs
@@ -4,6 +4,10 @@ using Mango.Services.OrderAPI.Data;
 using Mango.Services.OrderAPI.Service;
 using Mango.Services.OrderAPI.Service.IService;
 using Microsoft.EntityFrameworkCore;
+using Mango.Services.OrderAPI.Utility;
+using Mango.MessageBus;
+using Mango.Services.OrderAPI.Extensions;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,23 +21,39 @@ IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+builder.Services.AddHttpContextAccessor();
+
 
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IMessageBus, MessageBus>();
+
+
+builder.Services.AddScoped<BackendApiAuthenticationHttpClientHandler>();
+
+
+builder.Services.AddHttpClient("Product", u => u.BaseAddress =
+new Uri(builder.Configuration["ServiceUrls:ProductAPI"])).AddHttpMessageHandler<BackendApiAuthenticationHttpClientHandler>();
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.AddAppAuthetication();
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.MapScalarApiReference();
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
